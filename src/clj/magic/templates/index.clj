0dc5f678491019428ce3b64ec0a526234bdfca2b
dimovich <dimovich@gmail.com>
2016-12-05 16:08:28 +0200

    more work

--- src/clj/magic/templates/index.clj
+++ src/clj/magic/templates/index.clj
@@ -49,7 +49,7 @@
     ;;
     [:div.vspace-section1
      [:div.vcenter-section1
-      [:div.tt.centered.clickable {:onclick "window.location='menu#food';"}
+      [:div.ttr.centered.clickable {:onclick "window.location='menu#food';"}
        [:div.m-icon
         (svg-to-hiccup "html/assets/svg/plate.svg")]
        [:div.m-title
@@ -58,7 +58,7 @@
         [:span.m-text
          "Scopri le nostre proposte, prova i nostri piatti preparati con ingredienti freschi del territorio"]]]
 
-      [:div.tt.clickable {:onclick "window.location='menu#beer';"}
+      [:div.ttr.clickable {:onclick "window.location='menu#beer';"}
        [:div.m-icon
         (svg-to-hiccup "html/assets/svg/beer.svg")]
        [:div.m-title
@@ -67,7 +67,7 @@
         [:span.m-text
          "Vieni a degustare la nostra selezione di etichette italiane ed internazionali"]]]
 
-      [:div.tt.clickable {:onclick "window.location='menu#drinks';"}
+      [:div.ttr.clickable {:onclick "window.location='menu#drinks';"}
        [:div.m-icon
         (svg-to-hiccup "html/assets/svg/drinks.svg")]
        [:div.m-title
@@ -87,14 +87,15 @@
       "Un manipolo di folli che si sono detti: «Imbarchiamoci!». Così è nata «la nave dei folli», in omaggio al genio fiammingo del XV secolo, il pittore Jheronimus Bosch. Tuttavia, più che di pittura ci intendiamo di mangiare e bere: dall’esperienza di un pr duttore birraio, dalla competenza di un ristoratore incallito e dalla creatività del cuoco, a cui si aggiunge la professionalità di una figura esperta in comunicazione e marketing, è nata l’idea di un locale consacrato alla degustazione di birre rigorosamente artigianali, abbinate a pietanze tradizionali e piatti creativi. L’attenzione alle materie prime, la scelta di prodotti del territorio, provenienti da piccole realtà artigiane, costituiscono i nostri punti di forza. A tutte queste proposte abbiamo voluto aggiungere un’attrattiva culturale, invitando tutti i mesi artisti, attori di teatro, musicisti, sommelier, per creare serate a tema, eventi, concerti dal vivo e degustazioni."]
      [:div.c-img-container
       [:a.swipeboximg {:href "assets/img/chi_siamo_big.jpg"}
-       [:img.img-border {:src "assets/img/chi_siamo.jpg"}]]]]
+       [:img.desktop.img-border {:src "assets/img/chi_siamo.jpg"}]
+       [:img.mobile.img-border {:src "assets/img/chi_siamo_mobile.jpg"}]]]]
 
 
     ;;
     ;; EVENTI
     ;;
     [:div#events.vspace-full.vcenter-section3
-     [:p.text-center.section-title.overlay
+     [:p.section-title.overlay
       "EVENTI"]
      [:div.e-text-container.section-text.centered.text-center
       [:p.e-text
